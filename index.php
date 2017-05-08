92fe740f4c006d573a2ac041be69b81dca30ae7e
H34RT B34T <scientist_akm@icloud.com>
2017-05-09 04:23:33 +0630

    Programming section finished

--- index.php
+++ index.php
@@ -1,85 +1,93 @@
 <?php include 'header.php'; ?>
 
 <body>
-	<div class="navbar-fixed-top navbar navbar-default">
-		<div class="navbar-header">
-			<a href="#mynav" type="button" class="navbar-toggle">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</a>
-			<div class="navbar-brand">
-				<a href="index.php">
-					<span><img src="img/background.jpg"> TRI Organization</span>
+	<div class="container-fluid">
+		<div class="navbar-fixed-top navbar navbar-default">
+			<div class="navbar-header">
+				<a href="#mynav" type="button" class="navbar-toggle">
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
 				</a>
+				<div class="navbar-brand">
+					<a href="index.php">
+						<span><img src="img/background.jpg"> TRI Organization</span>
+					</a>
+				</div>
 			</div>
-		</div>
-		<div class="collapse navbar-collapse">
-			<ul class="navbar-nav nav navbar-right" id="mainNav">
-				<li class="active"><a href="index.php">HOME</a></li>
-				<li><a href="#">FORUM</a></li>
-				<li><a href="#">BLOG</a></li>
-				<li><a href="#">CHAT</a></li>
-				<li><a href="#">ABOUT US</a></li>
-				<li><a href="#">LOGIN</a></li>
-			</ul>
+			<div class="collapse navbar-collapse">
+				<ul class="navbar-nav nav navbar-right" id="mainNav">
+					<li class="active"><a href="index.php">HOME</a></li>
+					<li><a href="#">FORUM</a></li>
+					<li><a href="#">BLOG</a></li>
+					<li><a href="#">CHAT</a></li>
+					<li><a href="#">ABOUT US</a></li>
+					<li><a href="#">LOGIN</a></li>
+				</ul>
+			</div>
 		</div>
 	</div>
 	<nav id="mynav"></nav>
 
-	<div class="carousel slide" data-ride="carousel" id="mainCarousel">
-		<div class="carousel-inner">
-			<div class="item active">
-				<div class="carousel-caption">
-					<h2>What is TRI Organization ?</h2>
-					<h4>TRI Organization is Information Technology Organization and Who train Information Technology students to become professional. </h4>
+	<div class="container-fluid">
+		<div class="carousel slide" data-ride="carousel" id="mainCarousel">
+			<div class="carousel-inner">
+				<div class="item active">
+					<div class="carousel-caption">
+						<h2>What is TRI Organization ?</h2>
+						<h4>TRI Organization is Information Technology Organization and Who train Information Technology students to become professional. </h4>
+					</div>
+					<img src="img/background.jpg">
 				</div>
-				<img src="img/background.jpg">
-			</div>
-			<div class="item">
-				<div class="carousel-caption">
-					<h2>What is TRI Organizaton Aim ?</h2>
-					<h4>Our aim is To produce own projects, searh bugs, participate in Information Technonogy competition.</h4>
+				<div class="item">
+					<div class="carousel-caption">
+						<h2>What is TRI Organizaton Aim ?</h2>
+						<h4>Our aim is To produce own projects, searh bugs, participate in Information Technonogy competition.</h4>
+					</div>
+					<img src="img/background.jpg">
 				</div>
-				<img src="img/background.jpg">
-			</div>
-			<div class="item">
-				<div class="carousel-caption">
-					<h2>What is TRI Organizaton Aim ?</h2>
-					<h4>Our aim is To produce own projects, searh bugs, participate in Information Technonogy competition.</h4>
+				<div class="item">
+					<div class="carousel-caption">
+						<h2>What is TRI Organizaton Aim ?</h2>
+						<h4>Our aim is To produce own projects, searh bugs, participate in Information Technonogy competition.</h4>
+					</div>
+					<img src="img/background.jpg" alt="">
 				</div>
-				<img src="img/background.jpg" alt="">
-			</div>
-			<div class="item">
-				<div class="carousel-caption">
-					<h2>What is TRI Organization ?</h2>
-					<h4>TRI Organization is Information Technology Organization and Who train Information Technology students to become professional. </h4>
+				<div class="item">
+					<div class="carousel-caption">
+						<h2>What is TRI Organization ?</h2>
+						<h4>TRI Organization is Information Technology Organization and Who train Information Technology students to become professional. </h4>
+					</div>
+					<img src="img/background.jpg">
 				</div>
-				<img src="img/background.jpg">
 			</div>
-		</div>
 
-		<a href="#mainCarousel" class="left carousel-control" data-slide="prev">
-			<i class="fa fa-angle-left" aria-hidden="true"></i>
-		</a>
-		<a href="#mainCarousel" class="right carousel-control" data-slide="next">
-			<i class="fa fa-angle-right" aria-hidden="true"></i>
-		</a>
+			<a href="#mainCarousel" class="left carousel-control" data-slide="prev">
+				<i class="fa fa-angle-left" aria-hidden="true"></i>
+			</a>
+			<a href="#mainCarousel" class="right carousel-control" data-slide="next">
+				<i class="fa fa-angle-right" aria-hidden="true"></i>
+			</a>
+		</div>
 	</div>
 
 
 	<div class="container-fluid">
-		<div class="row">
-			<div class="aim">
-				<h6>WHY AND WHERE TRI ORGANIZATION ?</h6>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-					sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-				 	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-					nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
-					qui officia deserunt mollit anim id est laborum.
-				</p>
+		<div class="aim">
+			<div class="row">
+				<div class="layer">
+					<div class="col-md-8 col-sm-8 col-md-offset-2">
+						<h6>WHY AND WHERE TRI ORGANIZATION ?</h6>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+							sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						 	Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+							nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+							reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+							pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+							qui officia deserunt mollit anim id est laborum.
+						</p>
+					</div>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -88,7 +96,7 @@
 			<div class="row">
 				<div class="col-md-6 col-sm-6">
 					<div class="security-description">
-						<h6>TRI Organization and Information Security</h6>
+						<h6>Information Security</h6>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 						</p>
 					</div>
@@ -147,7 +155,7 @@
 				</div>
 				<div class="col-md-6 col-sm-6">
 					<div class="webdev-description">
-						<h6>TRI Organization and Web Developing</h6>
+						<h6>Web Developing</h6>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 						</p>
 					</div>
@@ -158,9 +166,70 @@
 	<div class="container-fluid">
 		<div class="software-engineering">
 			<div class="row">
-				<div class="col-md-12 col-sm-12">
-					<div class="se-detail">
-						<h5>TRI Organization and Software Enginnering</h5>
+				<div class="se-detail text-center">
+					<div class="se-part-section">
+						<div class="row">
+							<div class="col-md-4 col-sm-4 ">
+								<div class="se-part">
+									<div class="se-img">
+										<div class="carousel slide" data-ride="carousel" id="test-slide">
+											<div class="carousel-inner">
+												<div class="item active">
+													<img src="img/se.jpg">
+												</div>
+												<div class="item">
+													<img src="img/background.jpg">
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="se-letter">
+										<h6>Network Programming</h6>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<div class="se-part">
+									<div class="se-img">
+										<div class="carousel slide" data-ride="carousel" id="test-slide">
+											<div class="carousel-inner">
+												<div class="item active">
+													<img src="img/se.jpg">
+												</div>
+												<div class="item">
+													<img src="img/background.jpg">
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="se-letter">
+										<h6>Desktop Application Programming</h6>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4 col-sm-4">
+								<div class="se-part">
+									<div class="se-img">
+										<div class="carousel slide" data-ride="carousel" id="test-slide">
+											<div class="carousel-inner">
+												<div class="item active">
+													<img src="img/se.jpg">
+												</div>
+												<div class="item">
+													<img src="img/background.jpg">
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="se-letter">
+										<h6>Android Application Programming</h6>
+										<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore </p>
+									</div>
+								</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
